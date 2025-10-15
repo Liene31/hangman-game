2019f425b880eebdecff248c4dcee3e6e1f3aa10
Liene <l.bare@interface3.be>
2025-10-15 11:05:25 +0200

    update game logic and next rournd button

--- main.js
+++ main.js
@@ -12,14 +12,14 @@ const nextRoundBtn = document.getElementById("next-round");
 let wordToGuess;
 
 let wordToGuessArray = [];
-const missedLetterArray = [];
+let missedLetterArray = [];
 
 let guesses = 6;
 let wonScore = 0;
 let lostScore = 0;
 
 function drawDashes(wordToGuess) {
-  wordToGuessArray = []; // --> not sure I need to clear it
+  wordToGuessArray = [];
   for (let i = 0; i < wordToGuess.length; i++) {
     wordToGuessArray.push("__ ");
   }
@@ -58,6 +58,7 @@ checkBtn.addEventListener("click", () => {
     renderHtml(missedLetterArray, missedLettersSpan);
     if (guesses === 0) {
       checkBtn.disabled = true;
+      nextRoundBtn.disabled = false;
       lostScore++;
       lostScoreSpan.textContent = lostScore;
       gameStatusPara.textContent = `GAME OVER`;
@@ -66,6 +67,7 @@ checkBtn.addEventListener("click", () => {
   } else {
     if (!wordToGuessArray.includes("__ ")) {
       checkBtn.disabled = true;
+      nextRoundBtn.disabled = false;
       wonScore++;
       wonScoreSpan.textContent = wonScore;
       gameStatusPara.textContent = `YOU WON`;
@@ -78,23 +80,24 @@ checkBtn.addEventListener("click", () => {
 
 //Resets the game, except Won & Lost Statistics
 nextRoundBtn.addEventListener("click", () => {
-  guesses = 6;
-  gameStatusPara.textContent = `Guesses left: ${guesses}`;
-  missedLettersSpan.textContent = "";
   startGame();
 });
 
 function startGame() {
   const randomNumber = generateRandomNumber();
+  guesses = 6;
+  gameStatusPara.textContent = `Guesses left: ${guesses}`;
+  missedLettersSpan.textContent = "";
+  missedLetterArray = [];
   wordToGuess = wordsData[randomNumber];
   checkBtn.disabled = false;
+  nextRoundBtn.disabled = true;
   drawDashes(wordToGuess);
   renderHtml(wordToGuessArray, nameToGuessPara);
+
+  // ----------------------------------------------
   console.log(wordToGuess);
   console.log(wordToGuess.length);
 }
 
 startGame();
-
-// On the second round the Missed letters are not updating anymore
-// Need to clear the Missed Letter array, otherwise it's already with letters
